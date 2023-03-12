ba0ea0a5e6105665e0e46d04035a59e3880e48b1
unknown <mahdiiging@gmail.com>
2023-03-12 17:20:42 +0100

    config 2

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -7,5 +7,5 @@ const {
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    cacheDirectory: join(__dirname),
 };
