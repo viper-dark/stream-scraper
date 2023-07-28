baa4fd95c484113dad21bf0bee4b9a9a6a171221
unknown <mahdiiging@gmail.com>
2023-07-28 02:30:56 +0100

    other thing

--- dist/scraper.js
+++ dist/scraper.js
@@ -35,14 +35,20 @@ function matchData(day = "today") {
             : `${website}/match/${day}_matches.php`;
         //making the request to the url
         try {
-            //   const response = await axios.get(url);
-            // html = response.data;
-            const response = await fetch("https://www.kooora.com/default.aspx");
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const htmlContent = await response.text();
-            html = htmlContent;
+            const headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Cache-Control': 'no-cache',
+                'Connection': 'keep-alive', // Keep the connection alive
+            };
+            const requestOptions = {
+                method: 'GET',
+                headers: headers,
+            };
+            const response = await axios.get(url, { headers });
+            html = response.data;
             console.log(html);
         }
         catch (error) {
--- src/scraper.ts
+++ src/scraper.ts
@@ -39,20 +39,26 @@ function matchData(day = "today") {
     //making the request to the url
 
     try {
-   //   const response = await axios.get(url);
-     // html = response.data;
+     const headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36', // Use a common User-Agent string for Chrome
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', // Accept various content types
+      'Accept-Encoding': 'gzip, deflate, br', // Accept gzip, deflate, and brotli encoding
+      'Accept-Language': 'en-US,en;q=0.9', // Accept English language
+      'Cache-Control': 'no-cache', // Disable caching
+      'Connection': 'keep-alive', // Keep the connection alive
+    };
+    
+    const requestOptions = {
+      method: 'GET', // Change the method to match your request type (GET, POST, etc.)
+      headers: headers,
+    };
 
+      const response = await axios.get(url,{headers});
+     html = response.data;
+     console.log(html);
      
-        const response = await fetch("https://www.kooora.com/default.aspx");
-    
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-    
-        const htmlContent = await response.text();
-        html = htmlContent
-        console.log(html);
-        
+     
+     
 
     }
      catch (error) {
